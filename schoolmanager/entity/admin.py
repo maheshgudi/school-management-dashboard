426605be5d96464b127ec866b0594a06b4a123c6
maheshgudi <mahesh.p.gudi@gmail.com>
2020-02-16 00:47:19 +0530

    Add dummy data

--- schoolmanager/entity/admin.py
+++ schoolmanager/entity/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (
+	Teacher,
+	Student,
+	ClassRoom,
+	Subject,
+	Relative,
+	Chapter,
+	)
 
-# Register your models here.
+
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(ClassRoom)
+admin.site.register(Subject)
+admin.site.register(Relative)
+admin.site.register(Chapter)
--- schoolmanager/entity/models.py
+++ schoolmanager/entity/models.py
@@ -7,16 +7,24 @@ class Teacher(models.Model):
     full_name = models.CharField(max_length=40)
     date_of_joining = models.DateField()
     subject = models.ManyToManyField("Subject")
-    salary = models.FloatField(help_text="In LPA")
+    salary = models.FloatField(validators=[MinValueValidator(1.0)])
+    web_lecture = models.BooleanField(default=False) 
+
+    def __str__(self):
+        return f"Teacher: {self.full_name}"
 
 
 class Student(models.Model):
     full_name = models.CharField(max_length=40)
     date_of_joining = models.DateField()
     standard = models.PositiveSmallIntegerField(validators=[MaxValueValidator(1), MinValueValidator(10)])
-    roll_no = models.PositiveIntegerField()
+    roll_no = models.PositiveIntegerField(unique=True)
     rank = models.PositiveIntegerField()
     point_of_contact = models.ManyToManyField("Relative")
+
+    def __str__(self):
+        return f"Student: {self.full_name}"
+
 
 
 class ClassRoom(models.Model):
@@ -24,11 +32,19 @@ class ClassRoom(models.Model):
     web_lecture_support = models.BooleanField(default=False)
     shape = models.PositiveSmallIntegerField(choices=[(shape.value, shape.name) for shape in Shape])
 
+    def __str__(self):
+        return f"Classroom: {self.shape}"
+
 
 class Subject(models.Model):
+    name = models.CharField(max_length=40)
     chapters = models.ManyToManyField("Chapter")
     duration = models.IntegerField(default=30,validators=[MinValueValidator(30), MaxValueValidator(120)])
     total_duration = models.IntegerField()
+
+    def __str__(self):
+        return f"Subject: {self.name}"
+
 
 
 class Relative(models.Model):
@@ -36,6 +52,12 @@ class Relative(models.Model):
     contact_number = models.CharField(max_length=10)
     relation = models.CharField(max_length=40)
 
+    def __str__(self):
+        return f"Relative: {self.full_name}"
+
 
 class Chapter(models.Model):
     name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"Chapter: {self.name}"
