f2c93db362fdefe0137af24c098b984339acd9ce
maheshgudi <mahesh.p.gudi@gmail.com>
2020-02-16 18:54:08 +0530

    Fix base.html

--- schoolmanager/schoolmanager/urls.py
+++ schoolmanager/schoolmanager/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home/", TemplateView.as_view(template_name="entity/base.html")),
-    re_path(r"home/", include(entity_urls)),
+    path("home/", TemplateView.as_view(template_name="entity/base.html"), name="home"),
+    re_path(r"home/", include((entity_urls, "entity"), namespace="entity")),
 ]
